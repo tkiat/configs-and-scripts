1170b86c7bff1434a52934a3ae6bc8e456f197a2
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-08-17 18:08:45 +0700

    UPDATE

---
+++ configs/nix/configuration.nix
@@ -0,0 +1,132 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot = {
+    blacklistedKernelModules = [
+      "iwlwifi"
+    ];
+    kernelPackages = pkgs.linuxPackages;
+    # kernelPackages = pkgs.linuxPackages_latest-libre;
+    # kernelPackages = pkgs.linuxPackages-libre;
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        device = "/dev/sda";
+      };
+    };
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      ansible
+      bash
+      dmenu
+      firefox
+      gnupg
+      git
+      slock
+      st
+      vim_configurable
+      wget
+      xmobar
+    ];
+  };
+
+  hardware = {
+    # firmware = [
+    #   (
+    #     pkgs.runCommandNoCC "open-ath9k-htc" { } ''
+    #       mkdir -p $out/lib/firmware
+    #       cp ${./htc_9271.fw} $out/lib/firmware/htc_9271.fw
+    #     ''
+    #   )
+    # ];
+    pulseaudio.enable = true;
+    trackpoint.emulateWheel = true;
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  networking = {
+    hostName = "nixos";
+    interfaces.eno0.useDHCP = true;
+    networkmanager.enable = true;
+    useDHCP = false;
+  };
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  programs = {
+    slock.enable = true; # SUID wrappers
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+  };
+
+  powerManagement = {
+    cpuFreqGovernor = "ondemand";
+    enable = true;
+  };
+
+  services = {
+    openssh.enable = true;
+    printing.enable = true; # CUPS
+    xserver = {
+      enable = true;
+      displayManager = {
+        startx.enable = true;
+      };
+      layout = "us,th";
+      # libinput.enable = true; # touchpad support
+      windowManager = {
+        xmonad.enable = true;
+        xmonad.enableContribAndExtras = true;
+        xmonad.extraPackages = hpkgs: [
+          hpkgs.xmonad
+          hpkgs.xmonad-contrib
+          hpkgs.xmonad-extras
+        ];
+      };
+      xkbOptions = "grp:alt_space_toggle";
+    };
+  };
+
+  sound.enable = true;
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05";
+
+  time = {
+    timeZone = "Asia/Bangkok";
+  };
+
+  users = {
+    users.tkiat = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+    };
+  };
+}
+
