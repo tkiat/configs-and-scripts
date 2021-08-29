6231744ab7b65d8a671e1cf8eb26f5671092a7e6
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-08-29 11:48:40 +0700

    UPDATE

--- configs/nix/configuration-mbr.nix
+++ configs/nix/configuration-mbr.nix
@@ -3,6 +3,7 @@
 {
   imports =
     [
+      ./configuration/environment.nix
       ./hardware-configuration.nix
     ];
 
@@ -30,97 +31,6 @@
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      acpi
-      alsaUtils
-      ansible
-      bash
-      bat
-      calibre
-      cmus
-      cpufrequtils
-      curl
-      dejsonlz4
-      dmidecode
-      docker
-      docker-compose
-      du-dust
-      exa
-      fd
-      feh
-      flashrom
-      firefox
-      gcc
-      gdrive
-      ghc
-      ghostscript
-      gimp
-      git
-      gnome.zenity
-      gnumake
-      gnupg
-      gwenview
-      haskell-language-server
-      heimdall
-      htop
-      imagemagick
-      inkscape
-      jpegoptim
-      kdenlive
-      less
-      libreoffice
-      lm_sensors
-      loc
-      mc
-      mediainfo
-      mupdf
-      neofetch
-      neomutt
-      newsboat
-      nixpkgs-fmt
-      pandoc
-      pass
-      pencil
-      pinentry
-      purescript
-      python3
-      qview
-      ranger
-      redshift
-      ripgrep
-      rsync
-      scrot
-      shellcheck
-      simplescreenrecorder
-      smartmontools
-      spago
-      tdesktop
-      tealdeer
-      tmux
-      tokei
-      tor-browser-bundle-bin
-      trash-cli
-      tree
-      ungoogled-chromium
-      unzip
-      vim_configurable
-      vlc
-      vscodium
-      w3m
-      weechat
-      wget
-      xfce.exo
-      xfce.thunar
-      xfce.tumbler
-      xfce.xfconf
-      xmobar
-      xmonad-with-packages
-      yarn
-      zip
-    ];
   };
 
   hardware = {
