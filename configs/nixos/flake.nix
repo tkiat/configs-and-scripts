dfb92519819be60c2ebd8ecd81229338b4cd6e5d
tkiat <tkiat@tutanota.com>
2021-12-13 20:07:41 +0700

    UPDATE

--- configs/nixos/flake.nix
+++ configs/nixos/flake.nix
@@ -4,28 +4,7 @@
   outputs = { self, nixpkgs }: {
 
     nixosConfigurations.nixos-main = nixpkgs.lib.nixosSystem {
-      modules =
-        [
-          ./configuration.nix
-
-          ({ pkgs, ... }: {
-            boot.isContainer = true;
-
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "morty@example.org";
-            };
-          })
-        ];
+      modules = [ ./configuration.nix ];
     };
 
   };
