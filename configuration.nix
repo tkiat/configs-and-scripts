ae75d1b5fd2cd7beb208b13e669d9fe9ac6f6c13
tkiat <tkiat@tutanota.com>
2021-12-13 19:19:33 +0700

    UPDATE

--- configuration.nix
+++ configuration.nix
@@ -579,7 +579,7 @@ in
   i18n.defaultLocale = "en_US.UTF-8";
 
   networking = {
-    hostName = "nixos";
+    hostName = "nixos-main";
     interfaces.eno0.useDHCP = true;
     networkmanager.enable = true;
     useDHCP = false;
