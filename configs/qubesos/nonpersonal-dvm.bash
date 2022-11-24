8533cb04e834ca1feea777da6d734adea3f30ec3
tkiat <tkiat@tutanota.com>
2022-11-24 23:52:38 +0700

    UPDATE

--- configs/qubesos/nonpersonal-dvm.bash
+++ configs/qubesos/nonpersonal-dvm.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# fedora
+# setup fedora - run only once
 conf="/home/user/configs-and-scripts/configs"
 cp "$conf/xorg/.Xresources" ~ && xrdb -merge /home/user/.Xresources
 mkdir -p "/home/user/.local/share/fonts/comic shanns 2" && cp "$conf/font/comic-shanns/v2/comic shanns 2.ttf" "/home/user/.local/share/fonts/comic shanns 2" && fc-cache -v
 cp "$conf/qubesos/nonpersonal-dvm.alias" ~
+
+cp "$conf/ranger/rifle.conf-nonpersonal_dvm" "/home/user/.config/ranger/rifle.conf"
