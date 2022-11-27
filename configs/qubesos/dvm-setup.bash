686b369a1bdd9b7e1b69785b79e808e0271f6b1a
tkiat <tkiat@tutanota.com>
2022-11-27 11:05:53 +0700

    UPDATE

--- configs/qubesos/dvm-setup.bash
+++ configs/qubesos/dvm-setup.bash
@@ -3,7 +3,9 @@
 conf="/home/user/configs-and-scripts/configs"
 cp "$conf/xorg/.Xresources" ~ && xrdb -merge /home/user/.Xresources
 mkdir -p "/home/user/.local/share/fonts/comic shanns 2" && cp "$conf/font/comic-shanns/v2/comic shanns 2.ttf" "/home/user/.local/share/fonts/comic shanns 2" && fc-cache -v
-cp "$conf/qubesos/nonpersonal-dvm.alias" ~
 
-cp "$conf/ranger/rifle.conf-nonpersonal_dvm" "/home/user/.config/ranger/rifle.conf"
-mkdir -p /home/user/mnt/sda && mkdir /home/user/mnt/sdb && mkdir /home/user/mnt/sdc
+cp "$conf/alias/shared.alias" "/home/user/.shared.alias"
+cp "$conf/bash/.bashrc.shared" ~
+cp "$conf/ranger/rifle.conf" "/home/user/.config/ranger/rifle.conf"
+
+mkdir -p /home/user/mnt/sda && mkdir /home/user/mnt/sda1 && mkdir /home/user/mnt/sda2 && mkdir /home/user/mnt/sdb && mkdir /home/user/mnt/sdc
