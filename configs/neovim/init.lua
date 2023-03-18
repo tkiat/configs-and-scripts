a16f08bff425f1ae3a18208f6d18759910149d6f
tkiat <tkiat@tutanota.com>
2023-03-18 11:58:05 +0700

    UPDATE

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -1,12 +1,14 @@
-require('lsp')
+require("lsp")
 
 vim.cmd("let mapleader=','")
 
-vim.cmd('source ~/.config/nvim/vim/plugin.vim')
-vim.cmd('source ~/.config/nvim/vim/autofmtgen.vim')
-vim.cmd('source ~/.config/nvim/vim/comment.vim')
-vim.cmd('source ~/.config/nvim/vim/html.vim')
-vim.cmd('source ~/.config/nvim/vim/shared.vim')
-vim.cmd('source ~/.config/nvim/vim/completion.vim')
-vim.cmd('source ~/.config/nvim/vim/syntax.vim')
-vim.cmd('source ~/.config/nvim/vim/template.vim')
+vim.cmd("autocmd BufNewFile,BufRead * set colorcolumn=80")
+
+vim.cmd("source ~/.config/nvim/vim/plugin.vim")
+vim.cmd("source ~/.config/nvim/vim/autofmtgen.vim")
+vim.cmd("source ~/.config/nvim/vim/comment.vim")
+vim.cmd("source ~/.config/nvim/vim/html.vim")
+vim.cmd("source ~/.config/nvim/vim/shared.vim")
+vim.cmd("source ~/.config/nvim/vim/completion.vim")
+vim.cmd("source ~/.config/nvim/vim/syntax.vim")
+vim.cmd("source ~/.config/nvim/vim/template.vim")
