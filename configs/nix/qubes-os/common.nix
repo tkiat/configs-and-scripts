29b8e306ff4350b07ef2c1c34db27378fc84daa9
tkiat <tkiat@tutanota.com>
2023-07-13 13:56:52 +0700

    UPDATE

--- configs/nix/qubes-os/common.nix
+++ configs/nix/qubes-os/common.nix
@@ -33,7 +33,7 @@ in
 
         # SPLIT SSH CONFIGURATION >>>
         # replace "vault" with your AppVM name which stores the ssh private key(s)
-        SSH_VAULT_VM="offline"
+        SSH_VAULT_VM="vault"
 
         if [ "$SSH_VAULT_VM" != "" ]; then
           export SSH_AUTH_SOCK="/home/user/.SSH_AGENT_$SSH_VAULT_VM"
