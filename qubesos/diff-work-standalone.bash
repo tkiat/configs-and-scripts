b10600c1798fabee3a95fb0f03cf0389d70ef25c
Theerawat Kiatdarakun <theerawat@kiatdarakun.com>
2024-01-04 15:59:19 +0700

    UPDATE

--- qubesos/diff-work-standalone.bash
+++ qubesos/diff-work-standalone.bash
@@ -6,11 +6,12 @@ if ! [ -f "/home/$USER/.nix-profile/etc/profile.d/nix.sh" ]; then
 fi
 # --- flatpak
 if ! [ -x "$(command -v flatpak)" ]; then
-  echo installing flatpak ...
-  sudo apt install flatpak
+  echo installing flatpak ... && sudo apt install flatpak
+
   echo add flatpak remotes ...
   flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
   echo installing flatpak packages ...
-fi
   flatpak install --assumeyes flathub org.gimp.GIMP
   flatpak install --assumeyes flathub io.neovim.nvim
+fi
