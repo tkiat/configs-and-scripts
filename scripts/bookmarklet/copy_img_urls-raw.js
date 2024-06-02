fc065380f76909cf6c619fb4f07c8a0ef77cde23
Theerawat Kiatdarakun <theerawat@kiatdarakun.com>
2024-06-02 12:30:46 +0700

    UPDATE

--- scripts/bookmarklet/copy_img_urls-raw.js
+++ scripts/bookmarklet/copy_img_urls-raw.js
@@ -1,14 +1,10 @@
-javascript: (function() {
+javascript:(function() {
 /* fetch urls */
 const list=[];
 const a=document.getElementsByTagName('img');
 for (var i=0,l=a.length;i<l;i++) {
-    if (/\.(jpg|gif|png|jpeg)$/im.test(a[i].getAttribute('src')))
-    {
-        list.push(a[i].getAttribute('src'));
-    }
+  list.push("\"" + a[i].getAttribute('src') + "\"");
 }
-
 /* copy to clipboard */
 const dummy = document.createElement("textarea");
 document.body.appendChild(dummy);
@@ -17,3 +13,4 @@ dummy.select();
 document.execCommand("copy");
 document.body.removeChild(dummy);
 })()
+//     if (/\.(jpg|gif|png|jpeg)$/im.test(a[i].getAttribute('src')))
